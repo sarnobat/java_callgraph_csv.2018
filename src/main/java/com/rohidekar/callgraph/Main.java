7b3724b9d0a5e6b6c629501b79ed0baaf30fc068
Sridhar Sarnobat <sridhar.sarnobat@itsoninc.com>
2016-12-07 14:24:07 -0800

    Minor changes (untested)

--- src/main/java/com/rohidekar/callgraph/Main.java
+++ src/main/java/com/rohidekar/callgraph/Main.java
@@ -21,7 +21,7 @@ import javax.swing.tree.TreeModel;
  * @author ssarnobat@google.com (Sridhar Sarnobat)
  */
 public class Main {
-  private static Logger log = Logger.getLogger(Main.class);
+//  private static Logger log = Logger.getLogger(Main.class);
   // TODO (feature):add call relationship between 
   // TODO (feature): build containment hierarchy using import statements
   // TODO(feature): Order call graphs by depth
@@ -30,8 +30,8 @@ public class Main {
 
   // TODO: Create a configuration object. Have several profiles: automatic, maximal
   private static final boolean PRINT_CONTAINMENT = true;
-  private static final boolean PRINT_CALL_TREE = true;
-  private static final boolean PRINT_PACKAGE_ARCHITECTURE = true;
+//  private static final boolean PRINT_CALL_TREE = true;
+//  private static final boolean PRINT_PACKAGE_ARCHITECTURE = true;
 
 
   public static final int MIN_TREE_DEPTH = 3;
@@ -64,22 +64,22 @@ public class Main {
   private static void printGraphs(String resource) {
     Relationships relationships = new Relationships(resource);
     relationships.validate();
-    if (PRINT_CALL_TREE) {
-      Map<String, GraphNode> allMethodNamesToMethodNodes =
-          RelationshipToGraphTransformer.determineCallHierarchy(relationships);
-      relationships.validate();
-      Set<GraphNode> rootMethodNodes = RootFinder.findRootCallers(allMethodNamesToMethodNodes);
-      TreePrinter.printTrees(relationships, rootMethodNodes);
-    }
-    if (PRINT_PACKAGE_ARCHITECTURE) {
-      Map<String, GraphNodePackage> allPacakgeNamesToPackageNodes =
-          RelationshipToGraphTransformer.determinePackageStructure(relationships);
-      Set<GraphNode> rootMethodNodes = RootFinder.findRoots(allPacakgeNamesToPackageNodes);
-      if (log.isEnabledFor(Level.DEBUG)) {
-        log.debug("Root package: " + rootMethodNodes.iterator().next().toString());
-      }
-      TreePrinter.printTrees(rootMethodNodes);
-    }
+//    if (PRINT_CALL_TREE) {
+//      Map<String, GraphNode> allMethodNamesToMethodNodes =
+//          RelationshipToGraphTransformer.determineCallHierarchy(relationships);
+//      relationships.validate();
+//      Set<GraphNode> rootMethodNodes = RootFinder.findRootCallers(allMethodNamesToMethodNodes);
+//      TreePrinter.printTrees(relationships, rootMethodNodes);
+//    }
+//    if (PRINT_PACKAGE_ARCHITECTURE) {
+//      Map<String, GraphNodePackage> allPacakgeNamesToPackageNodes =
+//          RelationshipToGraphTransformer.determinePackageStructure(relationships);
+//      Set<GraphNode> rootMethodNodes = RootFinder.findRoots(allPacakgeNamesToPackageNodes);
+//      if (log.isEnabledFor(Level.DEBUG)) {
+//        log.debug("Root package: " + rootMethodNodes.iterator().next().toString());
+//      }
+//      TreePrinter.printTrees(rootMethodNodes);
+//    }
     System.out.println("Containment Hierarchy");
     if (PRINT_CONTAINMENT) {
       Map<String, GraphNode> classNameToClassNodes =
