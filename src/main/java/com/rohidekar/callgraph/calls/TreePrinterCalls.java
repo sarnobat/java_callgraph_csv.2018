3cb8e39251849811e272548640957f49bb7aae35
Sridhar Sarnobat <sridhar.sarnobat@itsoninc.com>
2016-12-12 22:58:14 -0800

    Minor changes (untested)

--- src/main/java/com/rohidekar/callgraph/calls/TreePrinterCalls.java
+++ src/main/java/com/rohidekar/callgraph/calls/TreePrinterCalls.java
@@ -5,6 +5,7 @@ import java.util.Set;
 
 import javax.swing.tree.TreeModel;
 
+import com.google.common.base.Objects;
 import com.google.common.collect.LinkedHashMultimap;
 import com.google.common.collect.Multimap;
 import com.rohidekar.callgraph.Main;
@@ -60,6 +61,7 @@ public class TreePrinterCalls {
       throw new IllegalAccessError();
     }
     for (GraphNode child : tn.getChildren()) {
+      System.out.println("\"" + tn.toString() + "\",\"" + child.toString() + "\"");
       printTreeTest(child, level + 1, visited);
     }
 
