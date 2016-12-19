d2e418d89dfdeac49ab8adcf71b43fe587d5bb8f
Sridhar Sarnobat <sridhar.sarnobat@itsoninc.com>
2016-12-19 15:58:25 -0800

    Minor changes (untested)

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 mvn --quiet clean compile
-mvn --quiet exec:java -Dexec.mainClass="com.rohidekar.callgraph.Main" | grep -v java | grep -v '\$' | grep -v 'build' | grep -v 'Futures' | grep -v 'Injector'
+#mvn --quiet exec:java -Dexec.mainClass="com.rohidekar.callgraph.Main" | grep -v java | grep -v '\$' | grep -v 'build' | grep -v 'Futures' | grep -v 'Injector'
+mvn --quiet exec:java -Dexec.mainClass="com.rohidekar.callgraph.Main"
