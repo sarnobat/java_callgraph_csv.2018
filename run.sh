fe9a1bf91b676333318d4307235ef8460811d8b9
Sridhar Sarnobat <sridhar.sarnobat@itsoninc.com>
2016-12-19 16:09:02 -0800

    Minor changes (untested)

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 mvn --quiet clean compile
 #mvn --quiet exec:java -Dexec.mainClass="com.rohidekar.callgraph.Main" | grep -v java | grep -v '\$' | grep -v 'build' | grep -v 'Futures' | grep -v 'Injector'
+
+# Don't do filtering here. Do it separately
 mvn --quiet exec:java -Dexec.mainClass="com.rohidekar.callgraph.Main"
