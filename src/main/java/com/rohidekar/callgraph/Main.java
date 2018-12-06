b815f4e3cc8148feb24785009021ece27db1dd08
Sridhar Sarnobat <ssarnobat@control4.com>
2018-12-06 11:42:24 -0800

    works

--- src/main/java/com/rohidekar/callgraph/Main.java
+++ src/main/java/com/rohidekar/callgraph/Main.java
@@ -2,14 +2,29 @@
 
 package com.rohidekar.callgraph;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import java.io.File;
+import java.io.IOException;
 import java.io.PrintStream;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 import javax.swing.tree.TreeModel;
+
+import org.apache.bcel.classfile.ClassFormatException;
+import org.apache.bcel.classfile.ClassParser;
+import org.apache.bcel.classfile.JavaClass;
+import org.apache.commons.collections.IteratorUtils;
+import org.apache.commons.io.FileUtils;
 
 import com.google.common.collect.LinkedHashMultimap;
 import com.google.common.collect.Multimap;
@@ -38,7 +53,8 @@ public class Main {
     } else {
       resource = args[0];
     }
-    RelationshipsMain relationships = new Relationships(resource);
+    RelationshipsMain relationships =
+        new Relationships(resource, getJavaClassesFromResource(resource));
     relationships.validate();
     Map<String, GraphNode> allMethodNamesToMethods = new LinkedHashMap<String, GraphNode>();
     // Create a custom call graph structure from the multimap (flatten)
@@ -116,6 +132,70 @@ public class Main {
     System.err.println(
         "Now use d3_helloworld_csv.git/singlefile_automated/ for visualization. For example: ");
     System.err.println("  cat /tmp/calls.csv | sh csv2d3.sh | tee /tmp/index.html");
+  }
+
+  @SuppressWarnings("resource")
+  public static Map<String, JavaClass> getJavaClassesFromResource(String resource) {
+    Map<String, JavaClass> javaClasses = new HashMap<String, JavaClass>();
+    boolean isJar = resource.endsWith("jar");
+    if (isJar) {
+      String zipFile = null;
+      zipFile = resource;
+      File jarFile = new File(resource);
+      if (!jarFile.exists()) {
+        System.out.println(
+            "JavaClassGenerator.getJavaClassesFromResource(): WARN: Jar file "
+                + resource
+                + " does not exist");
+      }
+      Collection<JarEntry> entries = null;
+      try {
+        entries = Collections.list(new JarFile(jarFile).entries());
+      } catch (IOException e) {
+        System.err.println("JavaClassGenerator.getJavaClassesFromResource() - " + e);
+      }
+      if (entries == null) {
+        System.err.println("JavaClassGenerator.getJavaClassesFromResource() - No entry");
+        return javaClasses;
+      }
+      for (JarEntry entry : entries) {
+        if (entry.isDirectory()) {
+          continue;
+        }
+        if (!entry.getName().endsWith(".class")) {
+          continue;
+        }
+        ClassParser classParser = isJar ? new ClassParser(zipFile, entry.getName()) : null;
+        if (classParser == null) {
+          System.err.println("JavaClassGenerator.getJavaClassesFromResource() - No class parser");
+          continue;
+        }
+        try {
+          JavaClass jc = classParser.parse();
+          javaClasses.put(jc.getClassName(), jc);
+        } catch (ClassFormatException e) {
+          e.printStackTrace();
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+    } else {
+      // Assume it's a directory
+      String[] extensions = {"class"};
+      Iterator<File> classesIter = FileUtils.iterateFiles(new File(resource), extensions, true);
+      @SuppressWarnings("unchecked")
+      Collection<File> files = IteratorUtils.toList(classesIter);
+      for (File aClass : files) {
+        try {
+          ClassParser classParser = new ClassParser(checkNotNull(aClass.getAbsolutePath()));
+          JavaClass jc = checkNotNull(checkNotNull(classParser).parse());
+          javaClasses.put(jc.getClassName(), jc);
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+    }
+    return javaClasses;
   }
 
   private static void printTreeTest(
