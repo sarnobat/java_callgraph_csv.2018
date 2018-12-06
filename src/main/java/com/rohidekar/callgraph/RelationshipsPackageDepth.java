c5e6a793cc04f94aef1d63744d6c6ed552754e8d
Sridhar Sarnobat <ssarnobat@control4.com>
2018-12-06 11:42:24 -0800

    works

--- src/main/java/com/rohidekar/callgraph/RelationshipsPackageDepth.java
+++
@@ -1,9 +0,0 @@
-package com.rohidekar.callgraph;
-
-import org.apache.bcel.classfile.JavaClass;
-import org.apache.commons.lang.ClassUtils;
-import org.apache.commons.lang.StringUtils;
-
-public class RelationshipsPackageDepth {
-
-}
